416c3bba8f3079d4e86c9763d2c068d9751cb9b8
Kévin Labesse <k.labesse@aforp.eu>
2015-02-09 13:34:20 +0100

    Form de création d'utilisateur pris en charge

--- GUI/createuser.cpp
+++ GUI/createuser.cpp
@@ -3,7 +3,8 @@
 
 CreateUser::CreateUser(QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::CreateUser)
+    ui(new Ui::CreateUser),
+    m_user()
 {
     ui->setupUi(this);
 }
@@ -11,4 +12,49 @@ CreateUser::CreateUser(QWidget *parent) :
 CreateUser::~CreateUser()
 {
     delete ui;
+}
+
+void CreateUser::loadUser(Entity::User user)
+{
+    m_user = user;
+
+    ui->nomLineEdit->setText(m_user.getName());
+    ui->prenomLineEdit->setText(m_user.getFirstName());
+    ui->emailLineEdit->setText(m_user.getEmail());
+    ui->administrateurCheckBox->setChecked(m_user.isAdmin());
+}
+
+void CreateUser::valideUser()
+{
+    m_user.setName(ui->nomLineEdit->text());
+    m_user.setFirstName(ui->prenomLineEdit->text());
+    m_user.setEmail(ui->emailLineEdit->text());
+
+    // Si l'utilisateur est nouveau ou si on rentre un nouveau mot de passe
+    if (m_user.getId() == 0 || !ui->passwordLineEdit->text().isEmpty())
+        m_user.setClearPassword(ui->passwordLineEdit->text());
+
+    m_user.setIsAdmin(ui->administrateurCheckBox->isChecked());
+
+    Utility::PersisterManager pm;
+
+    pm.persistOne(m_user);
+
+    this->close();
+}
+
+void CreateUser::formChange()
+{
+    QRegExp emailRegEx("\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b");
+            emailRegEx.setCaseSensitivity(Qt::CaseInsensitive);
+            emailRegEx.setPatternSyntax(QRegExp::RegExp);
+
+    if (!ui->nomLineEdit->text().isEmpty() &&
+        !ui->prenomLineEdit->text().isEmpty() &&
+        !ui->emailLineEdit->text().isEmpty() &&
+        !ui->passwordLineEdit->text().isEmpty() &&
+        emailRegEx.exactMatch( ui->emailLineEdit->text() ))
+            ui->validButton->setEnabled(true);
+    else
+        ui->validButton->setEnabled(false);
 }
--- GUI/createuser.hpp
+++ GUI/createuser.hpp
@@ -2,21 +2,31 @@
 #define CREATEUSER_HPP
 
 #include <QDialog>
+#include "../Model/Entity/User.hpp"
+#include "../Utilty/PersisterManager.hpp"
 
 namespace Ui {
-class CreateUser;
+    class CreateUser;
 }
 
 class CreateUser : public QDialog
 {
     Q_OBJECT
 
-public:
-    explicit CreateUser(QWidget *parent = 0);
-    ~CreateUser();
+    public:
+        explicit CreateUser(QWidget *parent = 0);
+        ~CreateUser();
 
-private:
-    Ui::CreateUser *ui;
+        void loadUser(Entity::User user);
+
+    public slots:
+        void valideUser();
+        void formChange();
+
+    private:
+        Ui::CreateUser *ui;
+
+        Entity::User m_user;
 };
 
 #endif // CREATEUSER_HPP
