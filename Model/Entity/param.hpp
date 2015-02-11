bdb0d657fbe5e6ce92390db363f8d2be7bc0b960
Kévin Labesse <k.labesse@aforp.eu>
2015-02-11 21:32:36 +0100

    Ajout entité Param

--- Model/Entity/param.hpp
+++ Model/Entity/param.hpp
@@ -9,15 +9,19 @@ namespace Entity
     {
         public:
             Param();
-            virtual ~Param();
 
             static const int weight();
 
-            void setValue(QString const& value);
-            void setIdConstraint(int id);
+            void setValue(QString const& value);// Change la valeur
+            void setIdConstraint(int id);       // Change l'id de la contrainte
 
-            QString getValue() const;
-            int getIdConstraint() const;
+            QString getValue() const;           // Retourne la valeur
+            int getIdConstraint() const;        // Retourne l'id de la contrainte associée
+            virtual int getWeight() const;      // Retourne le poids
+
+            virtual void persist() override;    // Persiste l'entité dans le base de données
+            virtual bool isValid() const;       // Retourne si l'entité est valide pour le base de données
+            virtual void remove() override;     // Supprime l'entité de la base de données
 
         private:
             QString m_value;
