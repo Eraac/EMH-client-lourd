9f2e1b5b028d2558632b3f5d591a2f5959c9344a
Labesse Kévin <k.labesse@aforp.eu>
2015-03-28 17:05:46 +0100

    Intégration de Tag avec la BDD

--- Model/Entity/Tag.cpp
+++ Model/Entity/Tag.cpp
@@ -24,31 +24,60 @@ QString Entity::Tag::getName() const
 
 Entity::Entity::ErrorType Entity::Tag::load(unsigned int id)
 {
-    // TODO
-    m_id = id;
-    m_name = "Demande";
+    if (!initDB())
+        return Entity::ErrorType::DATABASE_ERROR;
+
+    QSqlQuery query = m_db.exec("SELECT * FROM tag WHERE id = ?");
+
+    query.bindValue(0, id);
+    query.exec();
+
+    if (!query.first())
+        return Entity::ErrorType::NOT_FOUND;
+
+    m_id = query.value("id").toInt();
+    m_name = query.value("name").toString();
 
     return Entity::ErrorType::NONE;
 }
 
 Entity::Entity::ErrorType Entity::Tag::loadByName(const QString& name)
 {
-    // TODO
-    m_id = 5;
-    m_name = name;
+    if (!initDB())
+        return Entity::ErrorType::DATABASE_ERROR;
+
+    QSqlQuery query = m_db.exec("SELECT * FROM tag WHERE name = ?");
+
+    if (name.isEmpty())
+        return Entity::ErrorType::NOT_FOUND;
+
+    query.bindValue(0, name);
+
+    query.exec();
+
+    if (!query.first())
+        return Entity::ErrorType::NOT_FOUND;
+
+    m_id = query.value("id").toInt();
+    m_name = query.value("name").toString();
 
     return Entity::ErrorType::NONE;
 }
 
 QStringList Entity::Tag::getAll()
 {
-    // TODO
     QStringList listTag;
 
-    listTag.append("Administration");
-    listTag.append("Demande");
-    listTag.append("Problème");
-    listTag.append("Tag");
+    if (!initDB())
+        return listTag;
+
+    QSqlQuery query = m_db.exec("SELECT name FROM tag");
+
+    while (query.next())
+    {
+        QString tagname = query.value(0).toString();
+        listTag.append(tagname);
+    }
 
     return listTag;
 }
@@ -60,12 +89,51 @@ int Entity::Tag::getWeight() const
 
 void Entity::Tag::persist()
 {
-    // TODO
+    if (!initDB())
+        return; // TODO Add exception ?
+
+    // Si déjà une ID alors update
+    if (m_id != 0)
+    {
+        preUpdate();
+        QSqlQuery query = m_db.exec("UPDATE tag SET name = ? WHERE id = ?");
+
+        query.bindValue(0, m_name);
+        query.bindValue(1, m_id);
+        query.exec();
+
+        postUpdate();
+    }
+    else
+    {
+        preInsert();
+        QSqlQuery query = m_db.exec("INSERT INTO tag (name) VALUES(?)");
+
+        query.bindValue(0, m_name);
+        query.exec();
+
+        postInsert();
+
+        m_id = query.lastInsertId().toInt();
+    }
 }
 
 void Entity::Tag::remove()
 {
-    // TODO
+    if (!initDB())
+        return; // TODO Add exception ?
+
+    if (m_id == 0)
+        return; // TODO Add exception ?
+
+    preRemove();
+
+    QSqlQuery query = m_db.exec("DELETE FROM tag WHERE id = ?");
+    query.bindValue(0, m_id);
+
+    query.exec();
+
+    postRemove();
 }
 
 bool Entity::Tag::isValid() const
