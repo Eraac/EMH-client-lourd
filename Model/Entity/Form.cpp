749f2866f3607d2e3c94db00b3fdbc51154a932f
Labesse Kévin <k.labesse@aforp.eu>
2015-03-28 16:53:25 +0100

    Intégration de Form avec la BDD

--- Model/Entity/Form.cpp
+++ Model/Entity/Form.cpp
@@ -16,20 +16,47 @@ Entity::Form::~Form()
 
 bool Entity::Form::formExist(const QString &name)
 {
-    // TODO
-    return ("morphine" == name);
+    initDB();
+
+    QSqlQuery query = m_db.exec("SELECT COUNT(*) FROM form WHERE name = ?");
+
+    if (name.isEmpty() && !m_name.isEmpty())
+        query.bindValue(0, m_name);
+    else if (!name.isEmpty())
+        query.bindValue(0, name);
+    else
+        return false;
+
+    query.exec();
+    query.next();
+
+    int count = query.value(0).toInt();
+
+    return (count > 0) ? true : false;
 }
 
 Entity::Entity::ErrorType Entity::Form::load(unsigned int id)
 {
-    // TODO
-    m_id        = id;
-    m_name      = "Demande de béquilles";
-    m_description = "Pour faire une demande de béquilles";
-    m_info      = "Un médecin va venir les chercher";
-    m_important = "Il faut rentre les béquilles dès que possible";
-    m_status    = Form::Status::DEMAND;
-    m_idAuthor  = 1;
+    if (!initDB())
+        return Entity::ErrorType::DATABASE_ERROR;
+
+    QSqlQuery query = m_db.exec("SELECT * FROM form WHERE id = ?");
+
+    query.bindValue(0, id);
+    query.exec();
+
+    if (!query.first())
+        return Entity::ErrorType::NOT_FOUND;
+
+    m_id            = id;
+    m_name          = query.value("name").toString();
+    m_description   = query.value("description").toString();
+    m_info          = query.value("info").toString();
+    m_important     = query.value("important").toString();
+    m_color         = query.value("color").toString();
+    m_status        = query.value("status").toInt();
+    m_idAuthor      = query.value("author_id").toInt();
+
 
     return Entity::ErrorType::NONE;
 }
@@ -41,12 +68,75 @@ int Entity::Form::getWeight() const
 
 void Entity::Form::persist()
 {
-    // TODO
+    if (!initDB())
+        return; // TODO Add exception ?
+
+    // Si déjà une ID alors update
+    if (m_id != 0)
+    {
+        preUpdate();
+        QSqlQuery query = m_db.exec(
+                    "UPDATE form SET \
+                        author_id = ?, \
+                        name = ?, \
+                        description = ?, \
+                        info = ?, \
+                        important = ?, \
+                        status = ?, \
+                        color = ? \
+                    WHERE id = ?");
+
+        query.bindValue(0, m_idAuthor);
+        query.bindValue(1, m_name);
+        query.bindValue(2, m_description);
+        query.bindValue(3, m_info);
+        query.bindValue(4, m_important);
+        query.bindValue(5, m_status);
+        query.bindValue(6, m_color);
+        query.bindValue(7, m_id);
+        query.exec();
+
+        postUpdate();
+    }
+    else
+    {
+        preInsert();
+        QSqlQuery query = m_db.exec(
+                    "INSERT INTO form (author_id, name, description, info, important, status, color) \
+                    VALUES(?, ?, ?, ?, ?, ?, ?)"
+                    );
+
+        query.bindValue(0, m_idAuthor);
+        query.bindValue(1, m_name);
+        query.bindValue(2, m_description);
+        query.bindValue(3, m_info);
+        query.bindValue(4, m_important);
+        query.bindValue(5, m_status);
+        query.bindValue(6, m_color);
+        query.exec();
+
+        postInsert();
+
+        m_id = query.lastInsertId().toInt();
+    }
 }
 
 void Entity::Form::remove()
 {
-    // TODO
+    if (!initDB())
+        return; // TODO Add exception ?
+
+    if (m_id == 0)
+        return; // TODO Add exception ?
+
+    preRemove();
+
+    QSqlQuery query = m_db.exec("DELETE FROM form WHERE id = ?");
+    query.bindValue(0, m_id);
+
+    query.exec();
+
+    postRemove();
 }
 
 bool Entity::Form::isValid() const
