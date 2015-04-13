8ed1081d0027b2d7d9d9c18687ae99f8c1a35433
Labesse Kévin <k.labesse@aforp.eu>
2015-04-13 22:50:24 +0200

    Changement relation entre constraint et field
    ManyToOne au lieu de ManyToMany

--- Model/Entity/Constraint.cpp
+++ Model/Entity/Constraint.cpp
@@ -2,7 +2,7 @@
 
 const int Entity::Constraint::weight = 1;
 
-Entity::Constraint::Constraint() : Entity(), m_type(Constraint::Type::NONE)
+Entity::Constraint::Constraint() : Entity(), m_type(Constraint::Type::NONE), m_fieldId(0)
 {
 
 }
@@ -18,9 +18,80 @@ void Entity::Constraint::setType(const Constraint::Type type)
     m_type = type;
 }
 
+void Entity::Constraint::setFieldId(unsigned int id)
+{
+    m_fieldId = id;
+}
+
 Entity::Constraint::Type Entity::Constraint::getType() const
 {
     return m_type;
+}
+
+QString Entity::Constraint::getTypeReadable() const
+{
+    QString type;
+
+    switch(m_type)
+    {
+        case Constraint::Type::LENGTH:
+            type = "Longueur min et/ou max";
+        break;
+
+        case Constraint::Type::BETWEEN:
+            type = "Entre X et Y";
+        break;
+
+        case Constraint::Type::NOTEQUAL:
+            type = "Non égal";
+        break;
+
+        case Constraint::Type::LOWER:
+            type = "Plus petit que";
+        break;
+
+        case Constraint::Type::LOWEROREQUAL:
+            type = "Plus petit ou égal à";
+        break;
+
+        case Constraint::Type::GREATER:
+            type = "Plus grand que";
+        break;
+
+        case Constraint::Type::GREATEROREQUAL:
+            type = "Plus grand ou égal à";
+        break;
+
+        case Constraint::Type::USERPASSWORD:
+            type = "Mot de passe de l'utilisateur";
+        break;
+
+        case Constraint::Type::REGEX:
+            type = "Regex";
+        break;
+    }
+
+    return type;
+}
+
+QStringList Entity::Constraint::getParams()
+{
+    QStringList listParams;
+
+    if (!initDB())
+        return listParams;
+
+    QSqlQuery query = m_db.exec("SELECT value FROM param WHERE fieldConstraint_id = ?");
+
+    query.bindValue(0, m_id);
+    query.exec();
+
+    while (query.next())
+    {
+        listParams << query.value(0).toString();
+    }
+
+    return listParams;
 }
 
 Entity::Entity::ErrorType Entity::Constraint::load(unsigned int id)
@@ -38,6 +109,7 @@ Entity::Entity::ErrorType Entity::Constraint::load(unsigned int id)
 
     m_id = query.value("id").toInt();
     m_type = static_cast<Constraint::Type> (query.value("type").toInt());
+    m_fieldId = query.value("fields_id").toInt();
 
     return Entity::ErrorType::NONE;
 }
@@ -56,10 +128,11 @@ void Entity::Constraint::persist()
     if (m_id != 0)
     {
         preUpdate();
-        QSqlQuery query = m_db.exec("UPDATE fieldConstraint SET type = ? WHERE id = ?");
+        QSqlQuery query = m_db.exec("UPDATE fieldConstraint SET type = ?, fields_id = ? WHERE id = ?");
 
         query.bindValue(0, static_cast<int> (m_type));
-        query.bindValue(1, m_id);
+        query.bindValue(1, m_fieldId);
+        query.bindValue(2, m_id);
         query.exec();
 
         postUpdate();
@@ -67,9 +140,10 @@ void Entity::Constraint::persist()
     else
     {
         preInsert();
-        QSqlQuery query = m_db.exec("INSERT INTO fieldConstraint (type) VALUES(?)");
+        QSqlQuery query = m_db.exec("INSERT INTO fieldConstraint (type, fields_id) VALUES(?, ?)");
 
         query.bindValue(0, static_cast<int> (m_type));
+        query.bindValue(1, m_fieldId);
         query.exec();
 
         postInsert();
--- Model/Entity/Constraint.hpp
+++ Model/Entity/Constraint.hpp
@@ -20,8 +20,11 @@ namespace Entity
                          GREATER, GREATEROREQUAL};           // Type de contrainte
 
         void setType(Constraint::Type const type);      // Change le type
+        void setFieldId(unsigned int id);               // Change l'id du field
 
         Constraint::Type getType() const;               // Retourne le type
+        QString getTypeReadable() const;                // Retourne le type sous forme de string
+        QStringList getParams();                // Retourne les paramètres associés à la contrainte
 
         Entity::ErrorType load(unsigned int id);        // Charge l'entité depuis la base de données
 
@@ -33,6 +36,7 @@ namespace Entity
 
         private:
         Constraint::Type m_type;
+        unsigned int m_fieldId;
     };
 }
 
